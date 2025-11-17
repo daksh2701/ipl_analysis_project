0c89e36f0ea9390051ef4a1482dbd7f79a3464bf
Daksh Jain <139755032+daksh2701@users.noreply.github.com>
2025-11-18 00:33:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fe415f31-824b-4d52-957c-d591038d4961",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
